38de8cb8435ff9a0fb843875dfac4e62a36d4557
mmontelaro <135753734+mmontelaro@users.noreply.github.com>
2023-06-06 12:52:20 -0700

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,128 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
+from typing import Tuple
+import os
+import sys
+import torch
+import fire
+import time
+import json
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from pathlib import Path
+
+from fairscale.nn.model_parallel.initialize import initialize_model_parallel
+
+from llama.model import ModelArgs, Transformer
+from llama.tokenizer import Tokenizer
+from llama.generation import LLaMA
+
+
+def setup_model_parallel() -> Tuple[int, int]:
+    local_rank = int(os.environ.get("LOCAL_RANK", -1))
+    world_size = int(os.environ.get("WORLD_SIZE", -1))
+
+    torch.distributed.init_process_group("gloo")
+    initialize_model_parallel(world_size)
+    # torch.cuda.set_device(local_rank)
+    # torch.set_default_device(local_rank)
+
+    # seed must be the same in all processes
+    torch.manual_seed(1)
+    return local_rank, world_size
+
+
+def load(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    local_rank: int,
+    world_size: int,
+    max_seq_len: int,
+    max_batch_size: int,
+) -> LLaMA:
+    start_time = time.time()
+    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert world_size == len(
+        checkpoints
+    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+    ckpt_path = checkpoints[local_rank]
+    print("Loading")
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    print(checkpoint.keys())
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    model_args: ModelArgs = ModelArgs(
+        max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
+    )
+    tokenizer = Tokenizer(model_path=tokenizer_path)
+    model_args.vocab_size = tokenizer.n_words
+    torch.set_default_tensor_type(torch.FloatTensor)
+    model = Transformer(model_args)
+    torch.set_default_tensor_type(torch.FloatTensor)
+    model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+
+    generator = LLaMA(model, tokenizer)
+    print(f"Loaded in {time.time() - start_time:.2f} seconds")
+    return generator
+
+
+def main(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    temperature: float = 0.8,
+    top_p: float = 0.95,
+    max_seq_len: int = 512,
+    max_batch_size: int = 32,
+):
+    local_rank, world_size = setup_model_parallel()
+    if local_rank > 0:
+        sys.stdout = open(os.devnull, "w")
+
+    generator = load(
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+    )
+
+    while True:
+        prompt = input("Prompt (exit if you wish): ")
+        if prompt == 'exit':
+            break
+        results = generator.generate(
+            [prompt], max_gen_len=32, temperature=temperature, top_p=top_p
+        )
+
+        for result in results:
+            print(f"Result: \n{result}")
+            print("==================================\n")
+
+
+    prompts = [
+        # For these prompts, the expected answer is the natural continuation of the prompt
+        "Reference ",
+        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
+        """Tweet: "I hate it when my phone battery dies."
+        Sentiment: Negative
+        ###
+        Tweet: "My day has been 👍"
+        Sentiment: Positive
+        ###
+        Tweet: "This is the link to the article"
+        Sentiment: Neutral
+        ###
+        Tweet: "This new music video was incredibile"
+        Sentiment:""",
+                # """Translate English to French:
+
+        # sea otter => loutre de mer
+
+        # peppermint => menthe poivrée
+
+        # plush girafe => girafe peluche
+
+        # cheese =>""",
+    ]
+
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
